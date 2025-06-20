00c6ebed2f5d94034be4d75c179f89e6df40ee75
shadow79198 <williamrodriguez2607@gmail.com>
2025-06-20 14:07:02 -0400

    I hate CSS ._.

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ reason = document.querySelector("#reason")
 factor = 0
 const results = ["rushDown", "setUp", "heavy", "winCondition"]
 place = 0
+oneImg = document.querySelector(".imgOne")
+twoImg = document.querySelector(".imgTwo")
+questions = document.querySelector(".questions")
 
 //move between the screens
 //buttons to move
@@ -103,33 +106,44 @@ negative.addEventListener("click", function() {
 moveLast.addEventListener("click", function() {
     displayThreeQuestion.style.display = "none"
     displayResult.style.display = "block"
+    questions.style.display = "none"
     console.log(rushDown)
     console.log(setUp)
     console.log(heavy)
     console.log(winCondition)
+    //helps decide which group won
     const score = [rushDown, setUp, heavy, winCondition]
     for (let y=0; y<score.length; y++){
         if (score[place] < score[y]){
             place = y
             console.log(y)
-        }
+        }}
         console.log(place)
-    if (results[0] === results [y]){
+    
+        //gets the highest number of the questions
+    if (results[0] === results [place]){
         //rushdown
         selectedOption.innerHTML = "You should play a rushdown character"
-        reason.innerHTML = "rush down character focuses on pressuring the enemy with their combo potentials so that they are able to win the game in a fast pace playstyle. This gameplay style requieres people to spend some time practicing the character. Some characters like: Kazuya, Zero Suit Samus, Pikachu, etc."    
-    } else if (results[1] === results[y]){
+        reason.innerHTML = "rush down character focuses on pressuring the enemy with their combo potentials so that they are able to win the game in a fast pace playstyle. This gameplay style requieres people to spend some time practicing the character. Some characters like: Kazuya, Zero Suit Samus, Pikachu, etc."
+        oneImg.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHJ4dzVtdXU3bzlyZ2E3cjc2YWtwbXAweWcxZ3YwOG14cGZsdDE0NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/T9JZXEqRPLMadn21ZF/giphy.gif"
+        twoImg.src = "https://ssb.wiki.gallery/images/d/d7/Kirby_Up_B_SSBU.gif"    
+    } else if (results[1] === results[place]){
         //setUp
         selectedOption.innerHTML = "You should play a set up character"
         reason.innerHTML = "Set up characters focuses on keeping the stage under their control, with them putting traps on the field for the opponent to fall on it. Characters: like Snake, Simon/Richter, Link, etc. Fit this game style"
-    } else if (results[2] === results[y]){
+        oneImg.src = "https://ssb.wiki.gallery/images/7/7c/Snake_Down_B_SSBU.gif"
+        twoImg.src = "https://ssb.wiki.gallery/images/thumb/9/98/Link_Down_B_SSBU.gif/450px-Link_Down_B_SSBU.gif"
+    } else if (results[2] === results[place]){
         //heavy
         selectedOption.innerHTML = "You should play a heavy character"
         reason.innerHTML = "Heavy characters focuses on their raw damage potential without relying on learning the combos needed to dominate the game. Characters like: Ganondork, King DeDeDe, Ridley, etc. Are all examples of heavy characters"
+        oneImg.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDJzanpzMG01bThsMTBvdGN5ZHAzY24xbjVtMDNhaW80YW1xMXBhMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9Pvf8szcWOxdwp7a4g/giphy.gif"
+        twoImg.src = "https://ssb.wiki.gallery/images/9/94/Ganondorf_Neutral_B_SSBU.gif"
     } else{
         //winCondition
         selectedOption.innerHTML = "You should play a win condition character"
         reason.innerHTML = "Win condition characters rely on their strategy to prolong the game as much as they need to have their win condition activated. Characters like: Wario, Joker, Little Mac, etc. Take notice that these characters are good without the condition activated but once it is activated their kill potential increases by a lot."
-    }
+        oneImg.src = "https://ssb.wiki.gallery/images/f/f3/Wario_Down_B_SSBU.gif"
+        twoImg.src = "https://ssb.wiki.gallery/images/d/d0/SSBUJokerTaunt4.gif"
     }
 })
